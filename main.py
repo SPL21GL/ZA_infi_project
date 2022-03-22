d570187ff662ea02c6c0be2d1035bb310a387c88
taktischer <yougamer198@gmail.com>
2022-03-22 10:30:46 +0100

    commit, 22/03/2022

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from werkzeug.utils import redirect
+
 from controllers.index import index_blueprint
-from model.models import db
+from model.models import db, Book, Author, Publisher
+# from flask_wtf.csrf import CSRFProtect
 
 application = Flask(__name__)
 application.secret_key = "VerySecretSecretKey"
@@ -9,7 +12,35 @@ application.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 application.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost:8000/library"
 db.init_app(application)
 
-
 application.register_blueprint(index_blueprint)
 
-application.run()
+
+@application.route('/books', methods=['GET', 'POST'])
+def load_books():
+    books = db.session.query(Book).all()
+    print(books)
+    return render_template("books.html",
+                           title="Bibliothek ~ books",
+                           items=books)
+
+@application.route('/authors', methods=['GET', 'POST'])
+def load_authors():
+    authors = db.session.query(Author).all()
+    return render_template("author.html",
+                           title="Bibliothek ~ authors",
+                           items=authors)
+
+@application.route('/publishers', methods=['GET', 'POST'])
+def load_publishers():
+    publishers = db.session.query(Publisher).all()
+    return render_template("publishers.html",
+                           title="Bibliothek ~ publishers",
+                           items=publishers)
+
+@application.route('/data', methods=['GET', 'POST'])
+def edit_mode():
+    print("none")
+    return redirect("/")
+
+
+application.run(debug=True)
